e97206d473ddba4339074d09ff09c80426559dff
Fernando Contreras <fercreek@gmail.com>
2022-12-20 14:52:46 -0600

    Refactor quote model using syntax sugar

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,8 +3,19 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
+  # Way 1
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+
+  # Way 2
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+
+  # Way 3
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # Way 4
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
